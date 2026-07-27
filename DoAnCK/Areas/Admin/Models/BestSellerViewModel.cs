5da2bc6d3b4121c177922e78ee5da3d442abd6df
Cam Nguyen <hocamnguyen123@gmail.com>
2026-07-28 06:05:14 +0700

    Update StoreManagement n CustomerManagement

---
+++ DoAnCK/Areas/Admin/Models/BestSellerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnCK.Areas.Admin.Models
+{
+    public class BestSellerViewModel
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public string HinhAnh { get; set; }
+        public string TenDanhMuc { get; set; }
+        public decimal GiaBan { get; set; }
+        public int TongDaBan { get; set; }
+    }
+}
---
+++ DoAnCK/Areas/Admin/Models/CategorySalesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnCK.Areas.Admin.Models
+{
+    public class CategorySalesViewModel
+    {
+        public string TenDanhMuc { get; set; }
+        public int SoLuongBan { get; set; }
+        public double TyLePhanTram { get; set; }
+        public string ColorClass { get; set; } // Dùng để lưu màu CSS ngẫu nhiên hoặc theo thứ hạng
+    }
+}
